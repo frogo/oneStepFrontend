f9502c08b4810e16ca76d059c442440763afbb9f
frogo <66118181@qq.com>
2019-07-19 18:00:23 +0800

    feat: 公司存档

--- src/views/enterpriseCourseLibrary/index.vue
+++ src/views/enterpriseCourseLibrary/index.vue
@@ -2,42 +2,43 @@
   <el-main class="page-course">
     <div class="filter-box">
       <el-form ref="filterForm" :model="filterForm" label-width="80px" size="mini">
-        <el-form-item label="">
+        <el-form-item label="" class="keyword-input">
           <el-input
             v-model="filterForm.keyword"
             placeholder="请输入内容"
+            size="medium "
           >
             <i slot="suffix" class="el-input__icon el-icon-search" />
           </el-input>
         </el-form-item>
 
-        <el-form-item label="来源">
+        <el-form-item label="来源：">
           <el-radio-group v-model="filterForm.from">
             <el-radio :label="item" v-for="item in tags.from" border />
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="状态">
+        <el-form-item label="状态：">
           <el-radio-group v-model="filterForm.status">
             <el-radio :label="item" v-for="item in tags.status" border />
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="系列">
+        <el-form-item label="系列：">
           <el-radio-group v-model="filterForm.series">
             <el-radio :label="item" v-for="item in tags.series" border />
           </el-radio-group>
         </el-form-item>
 
-        <el-form-item label="层级">
+        <el-form-item label="层级：">
           <el-checkbox-group v-model="filterForm.level">
             <el-checkbox-button :label="item" v-for="item in tags.level" :key="item" name="type" />
           </el-checkbox-group>
         </el-form-item>
-        <el-form-item label="职能">
+        <el-form-item label="职能：">
           <el-checkbox-group v-model="filterForm.department">
             <el-checkbox-button :label="item" v-for="item in tags.department" name="type" />
           </el-checkbox-group>
         </el-form-item>
-        <el-form-item label="自定">
+        <el-form-item label="自定：">
           <el-checkbox-group v-model="filterForm.custom">
             <el-checkbox-button :label="item" v-for="item in tags.custom" name="type" />
           </el-checkbox-group>
