ac1094698da3507fe3d54fcf9bbe817d4a4622df
frogo <frogo>
2019-07-30 22:28:11 +0800

    feat: 创建课程 设置封面

--- src/views/enterpriseCourseLibrary/create.vue
+++ src/views/enterpriseCourseLibrary/create.vue
@@ -3,9 +3,9 @@
     <h2 class="pageName">
       创建课程
     </h2>
-    <el-form ref="createForm" :model="createForm" :rules="rules" label-width="100px" class="createForm">
-      <div class="title">
-        <span>基本信息</span>
+    <el-form ref="createForm" :model="createForm" label-width="100px" class="createForm">
+      <div class="head-line">
+        <span class="base-info">基本信息</span>
       </div>
       <el-form-item label="课程名称" prop="">
         <el-input v-model="createForm.courseName" placeholder="请输入" />
@@ -36,19 +36,24 @@
       <el-form-item label="课程封面" prop="" class="cover">
         <el-tabs v-model="activeTabName" @tab-click="handleTabClick" type="card">
           <el-tab-pane label="默认封面" name="default">
-            <ul>
-              <li><img src="../../assets/img/cover-1.png"> </li>
-              <li><img src="../../assets/img/cover-1.png"> </li>
-              <li><img src="../../assets/img/cover-1.png"> </li>
-              <li><img src="../../assets/img/cover-1.png"> </li>
-              <li><img src="../../assets/img/cover-1.png"> </li>
-              <li><img src="../../assets/img/cover-1.png"> </li>
-              <li><img src="../../assets/img/cover-1.png"> </li>
-              <li><img src="../../assets/img/cover-1.png"> </li>
-            </ul>
+            <el-radio-group v-model="radioCover">
+              <el-radio v-for="img in defaultCover" :label="img" @change="handleCoverClick">
+                <img :src="img">
+              </el-radio>
+            </el-radio-group>
           </el-tab-pane>
           <el-tab-pane label="自定义" name="custom">
-            配置管理
+            <div>尺寸：384*198 大小：2M 格式：JPG</div>
+            <el-upload
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload"
+              class="avatar-uploader"
+              action="https://jsonplaceholder.typicode.com/posts/"
+            >
+              <img v-if="imageUrl" :src="imageUrl" class="avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon" />
+            </el-upload>
           </el-tab-pane>
         </el-tabs>
       </el-form-item>
@@ -73,7 +78,18 @@ export default {
         intro: '',
         outline: ''
       },
-      activeTabName: 'default'
+      activeTabName: 'default',
+      radioCover: require('../../assets/img/cover-1.png'),
+      defaultCover: [
+        require('../../assets/img/cover-1.png'),
+        require('../../assets/img/cover-2.png'),
+        require('../../assets/img/cover-3.png'),
+        require('../../assets/img/cover-4.png'),
+        require('../../assets/img/cover-5.png'),
+        require('../../assets/img/cover-6.png'),
+        require('../../assets/img/cover-7.png'),
+        require('../../assets/img/cover-8.png')],
+      imageUrl: ''
     }
   },
   computed: {
@@ -85,6 +101,24 @@ export default {
   methods: {
     handleTabClick (tab, event) {
       console.log(tab, event)
+    },
+    handleCoverClick () {
+      console.log(this.radioCover)
+    },
+    handleAvatarSuccess (res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw)
+    },
+    beforeAvatarUpload (file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
     }
   }
 }
