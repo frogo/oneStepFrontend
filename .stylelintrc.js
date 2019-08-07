4465e74555f88aa417fcd6e278fd8979a9ed3e3c
frogo <66118181@qq.com>
2019-08-07 11:08:35 +0800

    feat: 测试styleLint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,6 +7,6 @@ module.exports = {
     ignorePseudoElements: ["v-deep"],
   } ],
     // 禁止低优先级的选择器出现在高优先级的选择器之后
-    // 'no-descending-specificity': null
+    'no-descending-specificity': null
 }
 }
