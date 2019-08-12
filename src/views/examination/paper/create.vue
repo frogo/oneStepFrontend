d33c2a31024601b9b7c0fa848c8425cd26f56cd1
frogo <66118181@qq.com>
2019-08-12 18:11:38 +0800

    feat: 试题创建-公司存档

--- src/views/examination/paper/create.vue
+++ src/views/examination/paper/create.vue
@@ -1,17 +1,167 @@
 <template>
   <el-main class="page-paperCreate">
-    创建试卷
+    <h2 class="pageName">
+      创建试卷
+    </h2>
+    <el-form ref="createForm" :model="createForm" :rules="rules" label-position="left" label-width="120px" class="createForm">
+      <div class="head-line">
+        <span class="base-info">基本信息</span>
+      </div>
+      <el-form-item label="试卷名称" prop="paperName">
+        <el-input v-model="createForm.paperName" placeholder="请输入" />
+      </el-form-item>
+      <el-form-item prop="qualifiedPercent" class="qualifiedPercent">
+        <div slot="label" class="label">
+          <span>合格分数比</span><el-popover
+            placement="top-start"
+            title="标题"
+            width="200"
+            trigger="hover"
+            content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"
+          >
+            <i slot="reference" class="el-icon-question" />
+          </el-popover>
+        </div>
+
+        <el-input v-model="createForm.qualifiedPercent" placeholder="请输入" class="w200" /> %
+      </el-form-item>
+
+      <div class="head-line">
+        <span class="base-info">考试设置
+          <el-popover
+            placement="top-start"
+            title="标题"
+            width="200"
+            trigger="hover"
+            content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"
+          >
+            <i slot="reference" class="el-icon-question" />
+          </el-popover>
+        </span>
+      </div>
+      <el-form-item label="考试时长" prop="hours">
+        <el-input v-model="createForm.hours" placeholder="请输入" class="w200" /> 分钟
+      </el-form-item>
+
+      <div class="inline-form">
+        <el-form-item>
+          <el-switch
+            v-model="createForm.randomSequence"
+            active-text="试题顺序随机"
+            active-color="#EF6520"
+            inactive-color="gray"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-switch
+            v-model="createForm.randomOption"
+            active-text="试题选项随机"
+            active-color="#EF6520"
+            inactive-color="gray"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-switch
+            v-model="createForm.noLookAnswer"
+            active-text="不许查看答案"
+            active-color="#EF6520"
+            inactive-color="gray"
+          />
+        </el-form-item>
+      </div>
+
+      <div class="head-line">
+        <span class="base-info">试题规则</span>
+      </div>
+
+      <el-form-item>
+        <el-select v-model="createForm.rules" placeholder="请选择" style="width:200px">
+          <el-option
+            v-for="item in paperRules"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </el-form-item>
+    </el-form>
+
+    <div class="operation-bar">
+      <el-button @click="handleSavePaperDraft" type="success">
+        保存草稿
+      </el-button>
+      <el-button @click="handleSavePaper" type="primary">
+        完成
+      </el-button>
+    </div>
   </el-main>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      createForm: {
+        paperName: '',
+        qualifiedPercent: '',
+        randomSequence: true,
+        randomOption: false,
+        noLookAnswer: false,
+        rules: 'manual'
+      },
+      rules: {
+        paperName: [
+          { required: true, message: '请输入试题名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        qualifiedPercent: [
+          { required: true, message: '请输入合格分数百分比数字', trigger: 'blur' }
+        ],
+        hours: [
+          { required: true, message: '请输入课时时长', trigger: 'blur' }
+        ]
+      },
+      paperRules: [{ label: '手动出题', value: 'manual' }, { label: '随机出题', value: 'random' }]
+    }
+  },
+  mounted: function () {
+  },
+  methods: {
+    handleSavePaperDraft () {
 
+    },
+    handleSavePaper () {
+
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 .page-paperCreate{
-  padding: 0;
+  box-shadow: 0px 0px 5px #888888;
+  margin: 15px auto;
+  padding: 20px;
+  h2.pageName{font-size: 26px}
+  .el-form.createForm{
+    .inline-form{
+      display: flex; justify-content:flex-start;
+      .el-form-item{ margin-right: 50px}
+    }
+    .head-line{margin: 25px 0;height:1px;position:relative;background:#f3f3f3;
+      span.base-info{display: inline-block;position: absolute;top:-10px;background: #fff;padding-right: 1em;color:#EF6520}
+      i{color:#8c939d;}
+    }
+    .el-input{width:55%}
+    .el-input.w200{width:200px}
+    .el-select.w200{width:200px}
+    .el-textarea{width:400px}
+    .qualifiedPercent{.label{display: inline-block;justify-content: flex-start;span{ margin-right: 5px;i{color:#8c939d;}}}}
+  }
+  .operation-bar{
+    display: flex;justify-content:center;
+    .el-button{width:100px}
+  }
+
 }
 </style>
--- src/views/examination/questions/create.vue
+++ src/views/examination/questions/create.vue
@@ -7,23 +7,65 @@
       <div class="head-line">
         <span class="base-info">基本信息</span>
       </div>
-      <el-form-item label="题库名称" prop="questionsName">
+      <el-form-item label="题库名称" prop="questionName">
         <el-input v-model="createForm.questionName" placeholder="请输入" />
       </el-form-item>
       <el-form-item label="题型选择" prop="questionType">
-        <el-input v-model="createForm.questionType" placeholder="请输入" />
+        <el-select v-model="createForm.questionType" placeholder="请选择">
+          <el-option
+            v-for="item in questionType"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
       </el-form-item>
-      <el-form-item label="选项数" prop="options">
-        <el-input v-model="createForm.options" placeholder="请输入" class="w200" /> <span>分钟</span>
-      </el-form-item>
+      <!--      <el-form-item label="选项数" prop="options">-->
+      <!--        <el-input v-model="createForm.optionsNumber" placeholder="请输入" class="w200" />-->
+      <!--      </el-form-item>-->
       <el-form-item label="题目" prop="questionTitle">
-        <el-input v-model="createForm.questionTitle" type="textarea" placeholder="请输入" class="w200" /> <span>分</span>
+        <el-input v-model="createForm.questionTitle" type="textarea" placeholder="请输入" class="w200" />
       </el-form-item>
       <div class="head-line">
         <span class="base-info">选项内容</span>
       </div>
-      <el-form-item prop="questionAnswer" />
+      <el-form-item prop="questionAnswer">
+        <el-table
+          ref="singleTable"
+          :data="createForm.options.questionOptionsData"
+          style="width: 100%"
+        >
+          <el-table-column
+            label="正确"
+            width="100"
+          >
+            <template slot-scope="scope">
+              <el-radio v-model="createForm.options.trueOption" :label="scope.row.option" />
+            </template>
+          </el-table-column>
+          <!--          <el-table-column-->
+          <!--            property="option"-->
+          <!--            label="编号"-->
+          <!--            width="120"-->
+          <!--          />-->
+          <el-table-column
+            property="name"
+            label="描述"
+          >
+            <template slot-scope="scope">
+              <el-input v-model="scope.row.content" type="textarea" />
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-form-item>
     </el-form>
+
+    <div class="operation-bar">
+      <el-button @click="submit" type="primary">
+        提交
+      </el-button>
+      <el-button>重置</el-button>
+    </div>
   </el-main>
 </template>
 
@@ -32,13 +74,45 @@ export default {
   name: 'Create',
   data () {
     return {
+      questionType: [
+        { label: '多选', value: 'multi' },
+        { label: '单选', value: 'signal' },
+        { label: '判断', value: 'trueFalse' }
+      ],
       createForm: {
         questionName: '',
         questionType: '',
-        options: [],
-        questionTitle: ''
+        optionsNumber: 4,
+        questionTitle: '',
+        options: {
+          trueOption: 'A',
+          questionOptionsData: [{ option: 'A', content: '' }, { option: 'B', content: '' }, { option: 'C', content: '' }, { option: 'D', content: '' }]
+        }
 
+      },
+      rules: {
+        questionName: [
+          { required: true, message: '请输入试题名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        questionType: [
+          { required: true, message: '请选择试题类型', trigger: 'blur' }
+        ],
+        options: [
+          { required: true, message: '请输入选项数', trigger: 'blur' }
+        ],
+        questionTitle: [
+          { required: true, message: '请输入题目', trigger: 'blur' }
+        ]
       }
+    }
+  },
+
+  mounted: function () {
+  },
+  methods: {
+    submit () {
+      console.log(this.createForm)
     }
   }
 }
@@ -46,6 +120,21 @@ export default {
 
 <style lang="scss">
 .page-questionCreate{
-  padding: 0;
+  box-shadow: 0px 0px 5px #888888;
+  margin: 15px auto;
+  padding: 20px;
+  h2.pageName{font-size: 26px}
+  .el-form.createForm{
+    .head-line{margin: 25px 0;height:1px;position:relative;background:#f3f3f3;
+      span.base-info{display: inline-block;position: absolute;top:-10px;background: #fff;padding-right: 1em;color:#EF6520}
+    }
+    .el-input{width:55%}
+    .el-input.w200{width:200px}
+    .el-textarea{width:400px}
+  }
+  .operation-bar{
+    display: flex;justify-content:center;
+    .el-button{width:100px}
+  }
 }
 </style>
